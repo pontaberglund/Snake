9af167b49e1f74700168b7815aa052d1d7c1d86f
Pontus <ponta.berglund.pb@gmail.com>
2022-12-20 21:18:11 +0100

    Add tail, apple and length. Add better updateScreen and movement stuff.

--- src/Frame.java
+++ src/Frame.java
@@ -2,11 +2,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Frame extends JFrame {
     JPanel[][] panels = null;
-    int headX, headY;
+    int[][] coordinate = new int[50][50];
+    int headX, headY, appleX, appleY, length;
+    char direction = 'u';
     Frame() {
         this.setTitle("Snake");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -15,6 +18,7 @@ public class Frame extends JFrame {
         this.setVisible(true);
 
         this.setLayout(null);
+        length = 1;
 
         //Create panels
         panels = new JPanel[50][50];
@@ -24,9 +28,9 @@ public class Frame extends JFrame {
                 panels[x][y].setBackground(Color.orange);
                 panels[x][y].setBounds(x*10,y*10,10,10);
                 this.add(panels[x][y]);
-                this.repaint();
             }
         }
+        setApple();
     }
     public void delay(int milliseconds) {
         try {
@@ -36,65 +40,127 @@ public class Frame extends JFrame {
         }
     }
     public void setHead(int x, int y) {
-        panels[x][y].setBackground(Color.black);
         headX = x;
         headY = y;
+        coordinate[x][y] = length;
+    }
+
+    public void setApple() {
+        Random rand = new Random();
+        appleX = rand.nextInt(50);
+        appleY = rand.nextInt(50);
+        panels[appleX][appleY].setBackground(Color.red);
+    }
+
+
+    public void updateScreen() {
+        for(int x = 0; x < panels.length; x++) {
+            for (int y = 0; y < panels[x].length; y++) {
+                if(coordinate[x][y] != 0)
+                    panels[x][y].setBackground(Color.black);
+                else if(x == appleX && y == appleY)
+                    panels[x][y].setBackground(Color.red);
+                else
+                    panels[x][y].setBackground(Color.orange);
+            }
+        }
         this.repaint();
     }
 
     public void moveUp() {
-        panels[headX][headY].setBackground(Color.orange);
-        panels[headX][headY-1].setBackground(Color.black);
         headY--;
-        this.repaint();
+        for(int x = 0; x < coordinate.length; x++) {
+            for (int y = 0; y < coordinate[x].length; y++) {
+                if(coordinate[x][y] != 0)
+                    coordinate[x][y]--;
+            }
+        }
+        setHead(headX, headY);
     }
 
     public void moveDown() {
-        panels[headX][headY].setBackground(Color.orange);
-        panels[headX][headY+1].setBackground(Color.black);
         headY++;
-        this.repaint();
+        for(int x = 0; x < coordinate.length; x++) {
+            for (int y = 0; y < coordinate[x].length; y++) {
+                if(coordinate[x][y] != 0)
+                    coordinate[x][y]--;
+            }
+        }
+        setHead(headX, headY);
     }
 
     public void moveRight() {
-        panels[headX][headY].setBackground(Color.orange);
-        panels[headX+1][headY].setBackground(Color.black);
         headX++;
-        this.repaint();
+        for(int x = 0; x < coordinate.length; x++) {
+            for (int y = 0; y < coordinate[x].length; y++) {
+                if(coordinate[x][y] != 0)
+                    coordinate[x][y]--;
+            }
+        }
+        setHead(headX, headY);
     }
 
     public void moveLeft() {
-        panels[headX][headY].setBackground(Color.orange);
-        panels[headX-1][headY].setBackground(Color.black);
         headX--;
-        this.repaint();
+        for(int x = 0; x < coordinate.length; x++) {
+            for (int y = 0; y < coordinate[x].length; y++) {
+                if(coordinate[x][y] != 0)
+                    coordinate[x][y]--;
+            }
+        }
+        setHead(headX, headY);
+    }
+
+    public void runGame() {
+        while(true) {
+            try {
+                if (direction == 'u')
+                    moveUp();
+                else if (direction == 'd')
+                    moveDown();
+                else if (direction == 'l')
+                    moveLeft();
+                else if (direction == 'r')
+                    moveRight();
+                if(headX == appleX && headY == appleY) {
+                    length++;
+                    panels[appleX][appleY].setBackground(Color.orange);
+                    setApple();
+                }
+                this.updateScreen();
+                delay(200);
+            }
+            catch (IndexOutOfBoundsException e) {
+                System.out.println("Dead");
+                break;
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
     }
 
     public void addInput() {
         this.addKeyListener(new KeyListener() {
             @Override
-            public void keyTyped(KeyEvent e) {
-
-            }
+            public void keyTyped(KeyEvent e) {}
 
             @Override
             public void keyPressed(KeyEvent e) {
                 if(e.getKeyChar() == 'w')
-                    moveUp();
+                    direction = 'u';
                 else if(e.getKeyChar() == 's')
-                    moveDown();
+                    direction = 'd';
                 else if(e.getKeyChar() == 'a')
-                    moveLeft();
+                    direction = 'l';
                 else if(e.getKeyChar() == 'd')
-                    moveRight();
+                    direction = 'r';
             }
 
             @Override
-            public void keyReleased(KeyEvent e) {
-
-            }
+            public void keyReleased(KeyEvent e) {}
         });
     }
 }
 
-//TODO Add check if head out of bounds, then die
+//TODO Add check if head collides with tail
+//TODO Better check if head out of bounds, then die
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         Frame frame = new Frame();
         frame.setHead(25,25);
-        for(int i = 0; i < 10; i++) {
-            frame.moveUp();
-            frame.delay(500);
-        }
         frame.addInput();
+        frame.runGame();
     }
 }
